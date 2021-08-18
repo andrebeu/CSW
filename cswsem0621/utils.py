0b10961bdfcfffc52971f0866c64ed648bca207f
andrebeu <aobeukers@gmail.com>
2021-08-18 12:30:15 -0400

    updated recording; refactored model from utils

--- cswsem0621/utils.py
+++ cswsem0621/utils.py
@@ -1,348 +1,114 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.special import softmax
+from itertools import product
+import time
+import seaborn as sns
+sns.set_context('talk')
 
-NSTATES = 9
+## import human data for fitting
+import pandas as pd
+hdf = pd.read_csv('../human_data.csv')
+humanB_acc,humanI_acc = hdf.loc[:,('blocked mean','interleaved mean')].values.T
 
+from model import *
 
-class SchemaTabularBayes():
-    """ CRP prior
-    tabluar predictive distirbution
-    """
+def get_sm(xth,norm=True):
+  """ 
+  given x_t_hat from subject
+  [trial,layer,node]
+  get 2afc normalized softmax for layer 2/3
+  return: [layer2/3,trial,node56/78]
+  norm=true 
+   apply softmax to xth
+   when prediction done with multiple schemas
+  """
+  nodes = {2:(5,6),3:(7,8)} 
+  L = [] # layer 2 and 3
+  for l,ns in nodes.items():
+    y = xth[:,l,ns]
+    if norm:
+      y=softmax(y,1)
+    L.append(y)
+  return np.array(L)
 
-    def __init__(self,concentration,stickiness_wi,stickiness_bt,sparsity,lrate=1,lratep=1,pvar=0):
-        self.Tmat = np.zeros([NSTATES,NSTATES])
-        self.alfa = concentration
-        self.beta_wi = stickiness_wi
-        self.beta_bt = stickiness_bt - np.abs(pvar*np.random.randn(1)[0])
-        self.lrate = lrate # lrate like
-        self.lratep = lratep # lrate prior
-        self.lmbda = sparsity
-        self.ntimes_sampled = 0
-        self.active = 0 # 1 if active on previous step
+def get_acc(data):
+  """ 
+  returns 2afc softmax of 
+  layer 2/3 transitions
+  single seed
+  """
+  ysm = get_sm(data['xth'])
+  L = []
+  for i in range(2):
+    ysml = ysm[i,:,:]
+    yt = data['exp'][:,i+3] 
+    pr_yt = ysml[range(len(ysml)),yt - (5+2*i)] # 
+    L.append(pr_yt)
+  return np.array(L)
 
-    def get_prior(self,betabt):
-        """ beta bt controls whether using beta_between or _within
-        """
-        if self.ntimes_sampled == 0:
-            return self.alfa
-        if betabt:
-            crp = self.lratep*self.ntimes_sampled + self.beta_bt*self.active
-        else:
-            crp = self.lratep*self.ntimes_sampled + self.beta_wi*self.active
-        return crp
-
-    def get_like(self,xtm1,xt):
-        PARAM_S = 2
-        num = self.lmbda + self.Tmat[xtm1,xt]
-        den = (PARAM_S*self.lmbda) + self.Tmat[xtm1,:].sum()
-        like = num/den
-        return like
-
-    def update(self,xtm1,xt):
-        self.Tmat[xtm1,xt]+=self.lrate
-        return None
-
-    def activate(self,flip):
-        if flip:
-            self.active = 1
-            self.ntimes_sampled += 1
-        else:
-            self.active = 0
-        return None
-
-    def predict(self,xtm1):
-        """ returns un-normalized count """
-        xthat = np.array([
-            self.get_like(xtm1,x) for x in range(NSTATES)
-            ])
-        return xthat
-
-
-class SEM():
-
-    def __init__(self,schargs):
-        self.SchClass = SchemaTabularBayes
-        self.schargs = schargs
-        self.init_schlib()
-
-    def init_schlib(self):
-        """ 
-        initialize with two schemas
-        one active one inactive
-        """
-        sch0 = self.SchClass(**self.schargs)
-        sch1 = self.SchClass(**self.schargs)
-        self.schlib = [sch0,sch1]
-        return None
-
-    def calc_posteriors(self,xtm1,xt,betabt=False,active_only=False):
-        """ loop over schema library
-        """
-        if active_only: # prediction
-            priors = [sch.get_prior(betabt) for sch in self.schlib if sch.ntimes_sampled>0]
-            likes = [sch.get_like(xtm1,xt) for sch in self.schlib if sch.ntimes_sampled>0]
-        else: # sch inference
-            priors = [sch.get_prior(betabt) for sch in self.schlib]
-            likes = [sch.get_like(xtm1,xt) for sch in self.schlib]
-        posteriors = [p*l for p,l in zip(priors,likes)]
-        return posteriors
-
-    def select_sch(self,xtm1,xt,betabt):
-        """ xt and xtm1 are ints
-        """
-        posteriors = self.calc_posteriors(xtm1,xt,betabt=betabt)
-        active_k = np.argmax(posteriors)
-        if active_k == len(self.schlib)-1:
-            self.schlib.append(self.SchClass(**self.schargs))
-        return active_k
-
-    def predict(self,xtm1):
-        """ 
-        """
-        pr_xt_z = np.array([
-            self.calc_posteriors(xtm1,x,active_only=True) for x in range(NSTATES)
-            ]) # probability of each next state under each schema
-        pr_xtp1 = np.mean(pr_xt_z,axis=1) # sum over schemas
-        # print(pr_xtp1)
-        return pr_xtp1
-
-    def run_exp(self,exp):
-        """ exp is L of trialL
-        trialL is L of obs (ints) 
-        """
-        ## recording
-        Mt0ij = np.zeros([len(exp),NSTATES,NSTATES])
-        data = {
-            'zt':-np.ones([len(exp),len(exp[0])]),
-            'xth':-np.ones([len(exp),len(exp[0]),NSTATES]),
-            'priors':[],
-            'likesL2':[],
-            'postL2':[],
-        }
-        ## 
-        scht = schtm = self.schlib[0] # sch0 is active to start
-        scht.activate(1)
-        for tridx,trialL in enumerate(exp):
-            if len(self.schlib)>=100:
-                return data
-            data['priors'].append([sch.get_prior(betabt=False) for sch in self.schlib])
-            data['likesL2'].append([sch.get_like(trialL[2],trialL[3]) for sch in self.schlib])
-            data['postL2'].append([self.calc_posteriors(trialL[2],trialL[3])])
-            for tstep,(xtm,xt) in enumerate(zip(trialL[:-1],trialL[1:])):
-                ## prediction: marginilize over schemas
-                xth = self.predict(xtm)
-                ## prediction: only active schema
-                # xth = scht.predict(xtm)
-                # update infered active schema
-                zt = self.select_sch(xtm,xt,betabt=tstep==0)
-                scht = self.schlib[zt]
-                # update transition matrix
-                scht.update(xtm,xt)
-                # update schema history
-                schtm.activate(0)
-                scht.activate(1)
-                schtm = scht
-                data['xth'][tridx][tstep] = xth
-                data['zt'][tridx][tstep] = zt       
-        return data
-
-
-
-class SchemaDeltaLearner():
-    """ no prior
-    tabluar learner 
-    delta rule updates randomly initialized distribution 
-    """
-    def __init__(self,init_lr=0.3,lr_decay_rate=0.1):
-        self.nstates = STSPACE_SIZE
-        # paramS
-        self.init_lr = init_lr  # fit
-        self.lr_decay_rate = lr_decay_rate # fit; larger faster decay 
-        # init objects
-        self.Tmat = self._init_transition_matrix()
-        self.nupdates = 1
-
-    def _init_transition_matrix(self):
-        # T[s0,s1] = pr(s1|s0)
-        T = np.random.random((self.nstates,self.nstates))
-        T = np.transpose(T/T.sum(axis=0)) # rows sum to one
-        return T
-
-    def calc_error_obs(self,st0,st1):
-        """ delta err vec is len Nstates
-        O(st0) - pr(st1), where pr(st1) 
-        is next state prediciton (softmax)
-        """
-        obs = np.zeros(self.nstates)
-        obs[st1] = 1
-        delta_err_vec = obs-self.Tmat[st0]
-        return delta_err_vec
-
-    def calc_error_on_path(self,path):
-        """ returns {st0: delta_err_vec} for st0 in path
-        """
-        D = {}
-        for st0,st1 in zip(path[:-1],path[1:]):
-            D[st0] = self.calc_error_obs(st0,st1)
-        return D
-
-    def update_sch(self,path):
-        lr=self.get_lr()
-        errD = self.calc_error_on_path(path)
-        for st0,errvec in errD.items():
-            self.Tmat[st0,:] += lr*errvec
-        self.nupdates += 1
-        return None
-
-    def get_lr(self):
-        return self.init_lr*np.exp(-self.lr_decay_rate*self.nupdates)
-
-    def eval(self):
-        """ 
-        eval schema response on all paths
-        returns [npaths,nsteps] 
-        where each entry is probability of correct response
-        """
-        task = Task()
-        paths = [item for sublist in task.paths for item in sublist]
-        acc_arr = []
-        for path in paths:
-            acc_arr.append(self.eval_path(path))
-        return np.array(acc_arr)
-
-    def eval_path(self,path):
-        accL = []
-        for s0,s1 in zip(path[:-1],path[1:]):
-            accL.append(self.Tmat[s0,s1])
-        return np.array(accL)
-
-    def calc_pe(self,path):
-        errD = self.calc_error_on_path(path)
-        pe = np.sum([i**2 for i in list(errD.values())])
-        return pe
-
-
-
-class Task():
+def unpack_acc(cbatch_data):
     """ 
+    given cbatch data (data from multiple curr and seeds)
+    return acc [curr,seed,trial]
     """
-
-    def __init__(self):
-        A1,A2,B1,B2 = self._init_paths_csw()
-        self.paths = [[A1,A2],[B1,B2]]
-        self.tsteps = len(self.paths[0][0])
-        self.exp_int = None
-        return None
+    accL = [] # curr
+    for cidx in range(len(cbatch_data)):
+        acc = np.array([get_acc(sbatch) for sbatch in cbatch_data[cidx]])
+        accL.append(acc.mean(1)) # mean over layers
+    return np.array(accL)
 
 
-    def _init_paths_csw(self):
-        """ 
-        begin -> locA -> node11, node 21, node 31, end
-        begin -> locA -> node12, node 22, node 32, end
-        begin -> locB -> node11, node 22, node 31, end
-        begin -> locB -> node12, node 21, node 32, end
-        """
-        begin = 0 # E0
-        locA,locB = 1,2 # E1
-        node11,node12 = 3,4 # E2 
-        node21,node22 = 5,6 # E3
-        node31,node32 = 7,8 # E4
-        end = 9
-        A1 = np.array([begin,locA,
-            node11,node21,node31
-            ])
-        A2 = np.array([begin,locA,
-            node12,node22,node32
-            ])
-        B1 = np.array([begin,locB,
-            node11,node22,node31
-            ])
-        B2 = np.array([begin,locB,
-            node12,node21,node32
-            ])
-        return A1,A2,B1,B2
+### RUN EXP
+def run_batch_exp(ns,args):
+  """ exp over seeds, 
+  single task_condition / param config
+  return full data
+  """
+  dataL = []
+  for i in range(ns):
+    task = Task()
+    sem = SEM(args['sch'])
+    exp,curr  = task.generate_experiment(**args['exp'])
+    data = sem.run_exp(exp)
+    data['exp']=exp
+    dataL.append(data)
+  return dataL
 
-    def _init_paths_toy(self):
-        """ 
-        begin -> locA -> node11, node 21, node 31, end
-        begin -> locA -> node12, node 22, node 32, end
-        begin -> locB -> node11, node 22, node 31, end
-        begin -> locB -> node12, node 21, node 32, end
-        """
-        locA,locB = 0,1
-        node11,node12 = 2,3
-        node21,node22 = 4,5
-        A1 = np.array([locA,
-            node11,node21
-            ])
-        A2 = np.array([locA,
-            node12,node22
-            ])
-        B1 = np.array([locB,
-            node11,node22
-            ])
-        B2 = np.array([locB,
-            node12,node21
-            ])
-        return A1,A2,B1,B2
+# def run_batch_exp_curr(ns,args,currL=['blocked','interleaved']):
+#   """ loop over task conditions, 
+#   return acc [task_condition,seed,trial]
+#   """
+#   L = []
+#   for curr in currL:
+#     args['exp']['condition'] = curr
+#     ## extract other data here
+#     dataL = run_batch_exp(ns,args)
+#     ##
+#     acc = np.array([get_acc(data) for data in dataL]).mean(1) # mean over layer
+#     L.append(acc)
+#   return np.array(L)
 
 
-    def get_curriculum(self,condition,n_train,n_test):
-        """ 
-        order of events
-        NB blocked: ntrain needs to be divisible by 4
-        """
-        curriculum = []   
-        if condition == 'blocked':
-            assert n_train%4==0
-            curriculum =  \
-                [0] * (n_train // 4) + \
-                [1] * (n_train // 4) + \
-                [0] * (n_train // 4) + \
-                [1] * (n_train // 4 )
-        elif condition == 'early':
-            curriculum =  \
-                [0] * (n_train // 4) + \
-                [1] * (n_train // 4) + \
-                [0, 1] * (n_train // 4)
-        elif condition == 'middle':
-            curriculum =  \
-                [0, 1] * (n_train // 8) + \
-                [0] * (n_train // 4) + \
-                [1] * (n_train // 4) + \
-                [0, 1] * (n_train // 8)
-        elif condition == 'late':
-            curriculum =  \
-                [0, 1] * (n_train // 4) + \
-                [0] * (n_train // 4) + \
-                [1] * (n_train // 4)
-        elif condition == 'interleaved':
-            curriculum = [0, 1] * (n_train // 2)
-        elif condition == 'single': ## DEBUG
-            curriculum =  \
-                [0] * (n_train) 
-        else:
-            print('condition not properly specified')
-            assert False
-        # 
-        curriculum += [int(np.random.rand() < 0.5) for _ in range(n_test)]
-        return np.array(curriculum)
+def run_batch_exp_curr2(ns,args,currL=['blocked','interleaved']):
+  """ loop over task conditions, 
+  return acc [task_condition,seed,trial]
+  """
+  accL = []
+  dataL = []
+  # dataD = {}
+  for curr in currL:
+    args['exp']['condition'] = curr
+    ## extract other data here
+    data_batch = run_batch_exp(ns,args)
+    dataL.append(data_batch)
+    # dataD[curr] = dataL
+    ## unpack seeds and take mean over layers
+    acc = np.array([get_acc(data) for data in data_batch]).mean(1) # mean over layer
+    accL.append(acc)
+  return dataL
+
+## plotting
 
 
-    def generate_experiment(self,condition,n_train,n_test):
-        """ 
-        exp: arr [ntrials,tsteps]
-        curr: arr [ntrials]
-        """
-        # get curriculum
-        n_trials = n_train+n_test
-        curr = self.get_curriculum(condition,n_train,n_test)
-        # generate trials
-        exp = -np.ones([n_trials,self.tsteps],dtype=int)
-        for trial_idx in range(n_train+n_test):
-            # select A1,A2,B1,B2
-            event_type = curr[trial_idx]
-            path_type = np.random.randint(2)
-            path_int = self.paths[event_type][path_type]
-            # embed
-            exp[trial_idx] = path_int
-        return exp,curr
+
