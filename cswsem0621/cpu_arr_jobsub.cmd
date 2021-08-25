fc9e43655d5c8ee919c6aedb34b78ba473af6062
andrebeu <aobeukers@gmail.com>
2021-08-25 10:13:11 -0400

    tiger

--- cswsem0621/cpu_arr_jobsub.cmd
+++ cswsem0621/cpu_arr_jobsub.cmd
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-#SBATCH -t 4:59:00   # runs for 48 hours (max)  
+#SBATCH -t 5:59:00   # runs for 48 hours (max)  
 #SBATCH -N 1         # node count 
-#SBATCH -c 1         # number of cores 
+#SBATCH -c 2         # number of cores 
 #SBATCH --mem 4000
 #SBATCH -o ./slurms/output.%j.%a.out
 
-
+## scotty
 # module load pyger/0.9
-conda init bash
-conda activate sem
+# conda init bash
+# conda activate sem
+## tiger
+module load anaconda3/4.4.0
+source activate sem
 
 # get arr idx
 slurm_arr_idx=${SLURM_ARRAY_TASK_ID}
