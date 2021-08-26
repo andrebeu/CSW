46252d099609cea49dc9b4b8743a560ae9259a53
andrebeu <aobeukers@gmail.com>
2021-08-26 18:41:25 -0400

    sample middle condition no CI

--- cswsem0621/rsync_data.cmd
+++ cswsem0621/rsync_data.cmd
@@ -3,4 +3,4 @@ echo "RSYNC GRIDSEARCH DATA FROM:"
 echo ${gsname}
 echo
 # rsync -r -vam --progress abeukers@tiger.princeton.edu:/home/abeukers/wd/CSW/cswsem0621/data/${gsname}/* /Users/abeukers/wd/csw/cswsem0621/data/${gsname}
-rsync -r -vam --progress abeukers@scotty.princeton.edu:/jukebox/norman/abeukers/sem/cswsem/CSW/cswsem0621/data/${gsname}/* /Users/abeukers/wd/csw/cswsem0621/data/${gsname}
+rsync -r -vam --progress --update abeukers@scotty.princeton.edu:/jukebox/norman/abeukers/sem/cswsem/CSW/cswsem0621/data/${gsname}/* /Users/abeukers/wd/csw/cswsem0621/data/${gsname}
